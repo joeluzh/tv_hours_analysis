21130aa6ee892de7de21b21817aff7884db8435f
joeluzh <joel.zoellig@uzh.ch>
2024-10-07 19:05:06 +0200

    started analysis

---
+++ main.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(here)
+
+tv_hourse_table <- gss_cat %>% 
+  group_by(marital) %>% 
+  summarise(mean_tv_hours = mean(tvhours, na.rm = TRUE))
+
+write_csv(tv_hourse_table, here("TV_Hours_by_Marital.csv"))
+
+
